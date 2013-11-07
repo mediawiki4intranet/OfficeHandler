60b9cbae46efbbe16bc46004b9f8ed058230a48a
Vitaliy Filippov <vitalif@yourcmc.ru>
2013-11-07 16:20:19 +0400

    tabs vs spaces, autoloading

--- OfficeHandler.class.php
+++ OfficeHandler.class.php
@@ -2,52 +2,10 @@
 
 class OfficeHandler extends ImageHandler
 {
-    public static $mimes = array(
-        'application/vnd.ms-excel',
-        'application/msword',
-        'application/vnd.oasis.opendocument.tex',
-        'application/vnd.oasis.opendocument.text-template',
-        'application/vnd.oasis.opendocument.graphics',
-        'application/vnd.oasis.opendocument.text',
-        'application/vnd.oasis.opendocument.text-template',
-        'application/vnd.oasis.opendocument.graphics',
-        'application/vnd.oasis.opendocument.graphics-template',
-        'application/vnd.oasis.opendocument.presentation',
-        'application/vnd.oasis.opendocument.presentation-template',
-        'application/vnd.oasis.opendocument.spreadsheet',
-        'application/vnd.oasis.opendocument.spreadsheet-template',
-        'application/vnd.oasis.opendocument.chart',
-        'application/vnd.oasis.opendocument.chart-template',
-        'application/vnd.oasis.opendocument.image',
-        'application/vnd.oasis.opendocument.image-template',
-        'application/vnd.oasis.opendocument.formula',
-        'application/vnd.oasis.opendocument.formula-template',
-        'application/vnd.oasis.opendocument.text-master',
-        'application/vnd.oasis.opendocument.text-web',
-        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-        'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
-        'application/vnd.ms-word.document.macroEnabled.12',
-        'application/vnd.ms-word.template.macroEnabled.12',
-        'application/vnd.openxmlformats-officedocument.presentationml.template',
-        'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
-        'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-        'application/vnd.ms-powerpoint.addin.macroEnabled.12',
-        'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
-        'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
-        'application/vnd.ms-powerpoint.slideshow.macroEnabled.12',
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
-        'application/vnd.ms-excel.sheet.macroEnabled.12',
-        'application/vnd.ms-excel.template.macroEnabled.12',
-        'application/vnd.ms-excel.addin.macroEnabled.12',
-        'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
-        'application/vnd.ms-xpsdocument',
-        'application/x-opc+zip',
-    );
-
     public function canRender($file)
     {
-        return in_array($file->mime, static::$mimes);
+        global $egOfficeHandlerMimeTypes;
+        return in_array($file->mime, $egOfficeHandlerMimeTypes);
     }
 
     public function doTransform($image, $dstPath, $dstUrl, $params, $flags = 0)
--- OfficeHandler.php
+++ OfficeHandler.php
@@ -27,8 +27,8 @@
 
 if (!defined('MEDIAWIKI'))
 {
-	echo "This file is an extension to the MediaWiki software and cannot be used standalone.\n";
-	die();
+    echo "This file is an extension to the MediaWiki software and cannot be used standalone.\n";
+    die();
 }
 
 $wgExtensionMessagesFiles['OfficeHandler'] = dirname(__FILE__).'/OfficeHandler.i18n.php';
@@ -37,30 +37,70 @@ $wgAutoloadClasses['OfficeAjax'] = dirname(__FILE__).'/OfficeAjax.class.php';
 $wgAjaxExportList[] = 'OfficeAjax::generatePreview';
 
 $wgExtensionCredits['parserhook'][] = array(
-	'name'    => 'MS Project file page',
-	'author'  => 'Vladimir Koptev',
-	'url'     => 'http://wiki.4intra.net/OfficeHandler',
-	'version' => '2013-08-30',
+    'name'    => 'Office document media handler and preview via LibreOffice',
+    'author'  => 'Vladimir Koptev',
+    'url'     => 'http://wiki.4intra.net/OfficeHandler',
+    'version' => '2013-08-30',
 );
-foreach (OfficeHandler::$mimes as $mime)
-{
-    $wgMediaHandlers[$mime] = 'OfficeHandler';
-}
 
 $wgResourceModules['OfficeHandler'] = array(
-	'scripts'       => array('Office.js'),
-	'styles'        => array(),
-	'dependencies'  => array('jquery'),
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'OfficeHandler',
-	'messages' => array(
-		'loading'
-	),
+    'scripts'       => array('Office.js'),
+    'localBasePath' => __DIR__,
+    'remoteExtPath' => 'OfficeHandler',
+    'messages'      => array('loading'),
 );
 $wgHooks['BeforePageDisplay'][] = 'efOfficeBeforePageDisplay';
 
 function efOfficeBeforePageDisplay(&$output, &$skin)
 {
-    $output->addModules( 'OfficeHandler' );
+    $output->addModules('OfficeHandler');
     return true;
+}
+
+$egOfficeHandlerMimeTypes = array(
+    'application/vnd.ms-excel',
+    'application/msword',
+    'application/vnd.oasis.opendocument.tex',
+    'application/vnd.oasis.opendocument.text-template',
+    'application/vnd.oasis.opendocument.graphics',
+    'application/vnd.oasis.opendocument.text',
+    'application/vnd.oasis.opendocument.text-template',
+    'application/vnd.oasis.opendocument.graphics',
+    'application/vnd.oasis.opendocument.graphics-template',
+    'application/vnd.oasis.opendocument.presentation',
+    'application/vnd.oasis.opendocument.presentation-template',
+    'application/vnd.oasis.opendocument.spreadsheet',
+    'application/vnd.oasis.opendocument.spreadsheet-template',
+    'application/vnd.oasis.opendocument.chart',
+    'application/vnd.oasis.opendocument.chart-template',
+    'application/vnd.oasis.opendocument.image',
+    'application/vnd.oasis.opendocument.image-template',
+    'application/vnd.oasis.opendocument.formula',
+    'application/vnd.oasis.opendocument.formula-template',
+    'application/vnd.oasis.opendocument.text-master',
+    'application/vnd.oasis.opendocument.text-web',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
+    'application/vnd.ms-word.document.macroEnabled.12',
+    'application/vnd.ms-word.template.macroEnabled.12',
+    'application/vnd.openxmlformats-officedocument.presentationml.template',
+    'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
+    'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+    'application/vnd.ms-powerpoint.addin.macroEnabled.12',
+    'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
+    'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
+    'application/vnd.ms-powerpoint.slideshow.macroEnabled.12',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
+    'application/vnd.ms-excel.sheet.macroEnabled.12',
+    'application/vnd.ms-excel.template.macroEnabled.12',
+    'application/vnd.ms-excel.addin.macroEnabled.12',
+    'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
+    'application/vnd.ms-xpsdocument',
+    'application/x-opc+zip',
+);
+
+foreach ($egOfficeHandlerMimeTypes as $mime)
+{
+    $wgMediaHandlers[$mime] = 'OfficeHandler';
 }
